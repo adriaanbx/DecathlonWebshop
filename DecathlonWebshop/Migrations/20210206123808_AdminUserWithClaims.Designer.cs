721ea638d10d93242b281ad67fcc97179a17aca6
Adriaan Buntinx <adriaan.buntinx@student.ehb.be>
2021-02-06 18:10:10 +0100

    Admin user seeded  with full control

--- DecathlonWebshop/Migrations/20210206123808_AdminUserWithClaims.Designer.cs
+++ DecathlonWebshop/Migrations/20210206123808_AdminUserWithClaims.Designer.cs
@@ -4,14 +4,16 @@ using DecathlonWebshop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DecathlonWebshop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210206123808_AdminUserWithClaims")]
+    partial class AdminUserWithClaims
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
